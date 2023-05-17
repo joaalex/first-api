d8fa82394ab8e60dd1417e6f3e7e375b27da8ad6
rosh <roshbon@gmail.com>
2023-05-17 16:17:42 +0100

    api journey

---
+++ dataStore.js
@@ -0,0 +1,21 @@
+const data = [
+    {
+        id: 1,
+        name: "John Doe",
+        email: "johndoe@gmail.com",
+        address: "7 Agunbiade street, Jakarta",
+        phone: "08123456789",
+        
+    },
+    {
+        id: 2,
+        name: "Ayodeji Ayorinde",
+        email: "ayokele@gmail.com",
+        address: "11, Wonuola street, Akoka",
+        phone: "09123456789",
+        
+    }
+]
+
+
+module.export =  data;
--- index.js
+++ index.js
@@ -1,26 +1,100 @@
-const http = require('node:http');
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const PORT = 4000
+const data = [
+    {
+        id: 1,
+        name: "John Doe",
+        email: "johndoe@gmail.com",
+        address: "7 Agunbiade street, Jakarta",
+        phone: "08123456789",
+        
+    },
+    {
+        id: 2,
+        name: "Ayodeji Ayorinde",
+        email: "ayokele@gmail.com",
+        address: "11, Wonuola street, Akoka",
+        phone: "09123456789",
+        
+    }
+]
 
-const hostname = 'localhost';
-const port = 1234;
+app.use(bodyParser.json())
 
-const server = http.createServer((req, res) => { 
-  
-       
-    res.setHeader('Content-Type', 'application/json');
-    res.write(JSON.stringify({
-        message: "Welcome to Node",
-        course: "Bsc. Computer Science",
-        level: "300"
-    }));
-    res.end();
+app.get('/', (req, res) => { 
+    res.json({
+        status: "success",
+        message: "Welcome to my API"
+    })
 })
 
-server.listen('1234', 'localhost',() => {
-    console.log(`Server running at ${hostname} on port ${port}`);
-});
 
 
-console.log("how far")
+app.get('/user', (req, res) => {
+    res.json({
+        status: "success",
+        message: "All Users",
+        data: data
+    })
+})
 
+
+app.post('/user/add', (req, res) => { 
+
+    const { name, email, phone, address } = req.body
+    if (!name || !email || !phone || !address || email.indexOf('@') === -1) { 
+        res.json({
+            status: "error",
+            message: "All fields are required"
+        })
+    }
+
+    const tempData = {
+        id: data.length + 1,
+        name,
+        email,
+        address,
+        phone,
+
+    }
+    data.push(tempData)
+    res.json({
+        status: "success",
+        message: "Data created successfully",
+        data: tempData
+    })
+})
+app.put('/user/update', (req, res) => {  
+   
+    const { id, name, phone, email, address } = req.body
+    if (!id || !name || !phone || !email || !address || email.indexOf('@') === -1)
+    { 
+        res.json({
+            status: "error",
+            message: "All fields are required"
+        })
+    }
+    const filteredData = data.filter(item => item.id === id)
+    // const filteredDataObj = filteredData[0]
+    filteredData[0].name = name
+    filteredData[0].phone = phone
+    filteredData[0].email = email
+    filteredData[0].address = address
+
+    res.json({
+        status: "success",
+        data : data
+    })
+})
+app.delete('/user/delete', (req, res) => {
+    
+})
+
+app.listen(PORT, () => { 
+
+    console.log(`Server is running on port ${PORT}`) 
+})
 
 
